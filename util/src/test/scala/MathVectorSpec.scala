2bb8fa531fc19edb31ffc8f7e5db2c7af7291d06
temeer <sharapudinov@gmail.com>
2015-07-07 15:16:27 +0300

    2

--- util/src/test/scala/MathVectorSpec.scala
+++ util/src/test/scala/MathVectorSpec.scala
@@ -51,5 +51,5 @@ class MathVectorSpec extends FlatSpec with Matchers {
   it should "apply unary plus correctly" in {
     (+MathVector(1.0, 1.0, 1.0)) should equal (MathVector(1.0, 1.0, 1.0))
   }
-  //little change
+  //little change  
 }
